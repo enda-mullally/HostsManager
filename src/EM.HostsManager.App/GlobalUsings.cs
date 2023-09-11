1f8131bfa6dba159009db19b99526c1b07ab89b6
Enda Mullally <enda.mullally@outlook.com>
2023-09-11 23:25:49 +0100

    Fixs

--- src/EM.HostsManager.App/GlobalUsings.cs
+++ src/EM.HostsManager.App/GlobalUsings.cs
@@ -2,7 +2,6 @@
 
 global using System;
 global using System.Diagnostics;
-global using System.Diagnostics.CodeAnalysis;
 global using System.IO;
 global using System.Reflection;
 global using System.Windows.Forms;
