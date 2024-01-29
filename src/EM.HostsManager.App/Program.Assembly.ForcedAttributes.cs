2cbeca09bc6d93e1b56dfe48ff114d5904037fb6
Enda M <55110345+enda-mullally@users.noreply.github.com>
2024-01-29 23:26:50 +0000

    Fix AssemblyTitle - Application title / display name (#31)
    * Update Installer sscript and add new deps for DI.
    
    * Fix file description (AssemblyTitle) - This is what's displayed in Task Manager. Long outstanding bug.

---
+++ src/EM.HostsManager.App/Program.Assembly.ForcedAttributes.cs
@@ -0,0 +1,6 @@
+﻿//
+// Copyright © 2024 Enda Mullally.
+//
+
+// Forced "Title" attribute (fixing an issue where the assembly filename was always auto generated and used regardless of the Title setting).
+[assembly: AssemblyTitle("Hosts Manager")] // Note: GenerateAssemblyTitleAttribute(false) setting in project file.
