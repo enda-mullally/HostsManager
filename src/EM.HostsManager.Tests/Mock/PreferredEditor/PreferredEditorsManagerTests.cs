e2dc67c73728b8bdf1a256ace561666c53c07312
Enda M <55110345+enda-mullally@users.noreply.github.com>
2024-04-16 23:28:14 +0100

    Mock test template (#47)
    * Initial mock test template. Will break this up a bit more later
    * Do a restore before running tests to reduce test output
    * Pre build [debug] before tests
    * Re-order

---
+++ src/EM.HostsManager.Tests/Mock/PreferredEditor/PreferredEditorsManagerTests.cs
@@ -0,0 +1,46 @@
+﻿using EM.HostsManager.PreferredEditor;
+using EM.HostsManager.Settings;
+
+namespace EM.HostsManager.Tests.Mock.PreferredEditor
+{
+    public class PreferredEditorsManagerTests
+    {
+        private static Mock<IEditor> GetMockPreferredEditor()
+        {
+            var mockEditor = new Mock<IEditor>();
+
+            mockEditor.SetupGet(p => p.DisplayName).Returns("Mock Editor");
+            mockEditor.SetupGet(p => p.IsDefault).Returns(true);
+            mockEditor.SetupGet(p => p.IsSelected).Returns(true);
+            mockEditor.SetupGet(p => p.Key).Returns("MockPreferredEditor");
+            mockEditor.Setup(e => e.Open()).Returns(true);
+
+            return mockEditor;
+        }
+
+        [Fact]
+        [Trait("Category", "Mock")]
+        public void PreferredEditor_Works()
+        {
+            // Arrange
+            var settingsProvider = new Mock<ISettingsProvider>();
+
+            settingsProvider.Setup(sp => sp.GetValue(It.IsAny<string>(), It.IsAny<string>())).Returns("MockPreferredEditor");
+            settingsProvider.Setup(sp => sp.SetValue(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+            var mockEditor = GetMockPreferredEditor();
+            var sut = new PreferredEditorManager(settingsProvider.Object).RegisterEditors([mockEditor.Object]);
+
+            // Act
+            sut.GetEditors();
+            sut.SaveSelectedEditor("Any");
+            var openResult = sut.Open();
+
+            // Assert
+            openResult.Should().BeTrue();
+            settingsProvider.Verify(sp => sp.GetValue(It.IsAny<string>(), It.IsAny<string>()), Times.AtLeastOnce);
+            settingsProvider.Verify(sp => sp.SetValue(It.IsAny<string>(), It.IsAny<string>()), Times.AtLeastOnce);
+            mockEditor.Verify(me => me.Open(), Times.AtLeastOnce);
+        }
+    }
+}
