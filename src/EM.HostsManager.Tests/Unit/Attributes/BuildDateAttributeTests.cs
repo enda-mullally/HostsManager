a443f4bde5ad08158aa84d0ca089d73b5bac5150
Enda M <55110345+enda-mullally@users.noreply.github.com>
2024-04-16 18:40:51 +0100

    Tests [Template] (#46)
    * Add Tests [Template]
    * Test yml for other test types?
    * Run tests on Debug build (default) & display a little more info

---
+++ src/EM.HostsManager.Tests/Unit/Attributes/BuildDateAttributeTests.cs
@@ -0,0 +1,16 @@
+﻿namespace EM.HostsManager.Tests.Unit.Attributes
+{
+    [Trait("Category", "Unit")]
+    public class BuildDateAttributeTests
+    {
+        [Fact]
+        public void BuildDateAttribute_Properties_Work()
+        {
+            const string buildDateString = "test build date";
+
+            var sut = new BuildDateAttribute(buildDateString);
+
+            sut.BuildDate.Should().BeEquivalentTo(buildDateString);
+        }
+    }
+}
